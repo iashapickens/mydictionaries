d30a2471ab628edad2b01cf61e43ddeac5247069
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2025-02-20 16:19:53 -0600

    updated school_report file

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -1,6 +1,6 @@
 """
 Process the JSON file named school_data.json. Display only those schools 
-that are part of the ACC, Big 12, Big Ten, Pac-12 and SEC divisons.
+that are part of the ACC, Big 12, Big Ten and SEC divisons.
 
 Copy that info here:
 
@@ -10,9 +10,10 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
 
-Display report for all universities that have a graduation rate for Women over 50%
-Display report for all universities that have a total price for in-state students living off campus over $50,000
+Display report for all universities that have a graduation rate for Women over 75%
+Display report for all universities that have a total price for in-state students living off campus over $60,000
 
 
 
 """
+
